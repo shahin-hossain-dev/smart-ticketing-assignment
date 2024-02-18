0fe8f1e2d786c32d107056bce8ecf0d9fc76401d
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-18 18:01:51 +0600

    selected seat count added

--- scripts/app.js
+++ scripts/app.js
@@ -20,6 +20,8 @@ function ticketMaxPurchase(element) {
             maxSeat.push(element.innerText);
             element.classList.add('bg-[#1DD100]');
             element.classList.add('text-white');
+            totalSeatCount();
+            setSelectedSeat();
         }
         else {
             alert("You can't buy over 4 tickets");
@@ -30,8 +32,27 @@ function ticketMaxPurchase(element) {
     }
 }
 
+function setSelectedSeat() {
+    const selectedSeat = getElementValueById('selected-seat');
+    const updateSelectedSeat = selectedSeat + 1;
+    setElementValueById('selected-seat', updateSelectedSeat)
+}
+
+function totalSeatCount() {
+    const totalSeat = getElementValueById('total-seat');
+    const updateSeat = totalSeat - 1;
+    setElementValueById('total-seat', updateSeat);
+}
+
+function setElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+
+}
+
 function getElementValueById(elementId) {
     const element = document.getElementById(elementId);
     const value = parseInt(element.innerText);
     return value;
 }
+
