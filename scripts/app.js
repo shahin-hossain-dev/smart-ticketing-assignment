b0e825040c7a21aee7b096f5390f73e873dbb6cb
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-18 20:47:30 +0600

    ticket total count fuctionality added, table responsive update

--- scripts/app.js
+++ scripts/app.js
@@ -4,38 +4,74 @@ const maxSeat = [];
 for (const seat of allSeat) {
     seat.addEventListener('click', function (event) {
         const element = event.target;
-        ticketMaxPurchase(element);
         const elementId = element.innerText;
 
+        if (maxSeat.includes(element.innerText) === false) {
 
+            if (maxSeat.length < 4) {
+                maxSeat.push(element.innerText);
+                element.classList.add('bg-[#1DD100]');
+                element.classList.add('text-white');
+                totalSeatCount();
+                setSelectedSeat();
+                setTicketList(element);
+                totalPriceCount();
+
+            }
+            else {
+                alert("You can't buy over 4 tickets");
+            }
+        }
+        else {
+            alert("This seat already sold")
+        }
 
     })
 }
 
-function ticketMaxPurchase(element) {
-
-    if (maxSeat.includes(element.innerText) === false) {
-
-        if (maxSeat.length < 4) {
-            maxSeat.push(element.innerText);
-            element.classList.add('bg-[#1DD100]');
-            element.classList.add('text-white');
-            totalSeatCount();
-            setSelectedSeat();
-        }
-        else {
-            alert("You can't buy over 4 tickets");
-        }
+document.getElementById('btn-discount').addEventListener('click', function () {
+    const couponCode = getInputValueById('coupon-field')
+    if (couponCode === 'NEW15') {
+        console.log('15% discount')
     }
-    else {
-        alert("This seat already sold")
+    else if (couponCode === 'Couple20') {
+        console.log('20% discount')
     }
+
+})
+
+function getInputValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const value = element.value;
+    return value;
 }
 
+
+function totalPriceCount() {
+    const total = getElementValueById('total');
+    const ticketPrice = getElementValueById('ticket-price');
+    const updatedTotal = total + ticketPrice;
+    setElementValueById('total', updatedTotal);
+}
+
+function setTicketList(element) {
+    const selectedContainer = document.getElementById('selected-container');
+    const perTicketPrice = getElementValueById('per-ticket-price');
+    const tr = document.createElement('tr');
+    tr.classList.add('text-[#03071299]', 'text-base')
+    tr.innerHTML = `
+        <td>${element.innerText}</td>
+       <td>Economy</td>
+       <td id="ticket-price" class="text-right">${perTicketPrice}</td>
+    `
+    selectedContainer.appendChild(tr);
+}
+
+
 function setSelectedSeat() {
-    const selectedSeat = getElementValueById('selected-seat');
+    const selectedSeat = getElementValueById('selected-seat-count');
     const updateSelectedSeat = selectedSeat + 1;
-    setElementValueById('selected-seat', updateSelectedSeat)
+    setElementValueById('selected-seat-count', updateSelectedSeat)
 }
 
 function totalSeatCount() {
