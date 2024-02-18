cd855d1e885a50269178d0903b13d88297fee930
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-18 17:15:00 +0600

    seat selection functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,37 @@
-console.log('app.js')
+
+const allSeat = document.getElementsByClassName('btn-seat');
+const maxSeat = [];
+for (const seat of allSeat) {
+    seat.addEventListener('click', function (event) {
+        const element = event.target;
+        ticketMaxPurchase(element);
+        const elementId = element.innerText;
+
+
+
+    })
+}
+
+function ticketMaxPurchase(element) {
+
+    if (maxSeat.includes(element.innerText) === false) {
+
+        if (maxSeat.length < 4) {
+            maxSeat.push(element.innerText);
+            element.classList.add('bg-[#1DD100]');
+            element.classList.add('text-white');
+        }
+        else {
+            alert("You can't buy over 4 tickets");
+        }
+    }
+    else {
+        alert("This seat already sold")
+    }
+}
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const value = parseInt(element.innerText);
+    return value;
+}
